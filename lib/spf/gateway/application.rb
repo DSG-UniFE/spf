5883b0c99e2210194190c0e41993e31baafe87a2
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-07-21 17:08:38 +0200

    Added dynamic definition of the linear_decay and distance_decay methods; calls to java code to interface with the DisService proxy still missing

--- lib/spf/gateway/application.rb
+++ lib/spf/gateway/application.rb
@@ -1,18 +1,46 @@
+require 'java'
+
 module SPF
   module Gateway
     class Application
+      
       attr_reader :priority
 
-      def initialize(priority, ...)
+      def initialize(priority, time_decay_manner, distance_decay_manner, time_decay_constant, distance_decay_constant)
         @priority = priority
+        @time_decay_constant = time_decay_constant
+        @distance_decay_constant = distance_decay_constant
+        
+        if time_decay_manner == "exponential"
+          define_method("time_decay") do |initial_value, elapsed_time|
+            Decays.exponential_decay(initial_value, elapsed_time, @time_decay_constant)
+          end
+        elsif time_decay_manner == "linear"
+          define_method("time_decay") do |initial_value, elapsed_time, decay_constant|
+            Decays.linear_decay(initial_value, elapsed_time, @time_decay_constant)
+          end
+        else
+          raise 'Time decay #{time_decay_manner} manner not recognized'
+        end
+        if distance_decay_manner == "exponential"
+          define_method("distance_decay") do |initial_value, elapsed_time|
+            Decays.exponential_decay(initial_value, elapsed_time, @distance_decay_constant)
+          end
+        elsif distance_decay_manner == "linear"
+          define_method("distance_decay") do |initial_value, elapsed_time|
+            Decays.linear_decay(initial_value, elapsed_time, @distance_decay_constant)
+          end
+        else
+          raise 'Distance decay #{distance_decay_manner} manner not recognized'
+        end
       end
-
-      def time_decay
-        # TODO: implement
+      
+      def time_decay(initial_value, elapsed_time)
+         raise 'Method time_decay not implemented'
       end
-
-      def distance_decay
-        # TODO: implement
+      
+      def distance_decay(initial_value, elapsed_time)
+         raise 'Method distance_decay not implemented'
       end
 
       def disseminate
