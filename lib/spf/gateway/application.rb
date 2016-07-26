98fa062f23109e8ef69f5173c009a0c3db9b2d99
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-07-26 10:41:52 +0200

    Refactoring of Application class.

--- lib/spf/gateway/application.rb
+++ lib/spf/gateway/application.rb
@@ -3,25 +3,23 @@ require 'java'
 module SPF
   module Gateway
     class Application
-      
+
       attr_reader :priority
 
       def initialize(priority, time_decay_manner, distance_decay_manner, time_decay_constant, distance_decay_constant)
         @priority = priority
         @time_decay_constant = time_decay_constant
         @distance_decay_constant = distance_decay_constant
-        
-        if time_decay_manner == "exponential"
-          define_method("time_decay") do |initial_value, elapsed_time|
-            Decays.exponential_decay(initial_value, elapsed_time, @time_decay_constant)
-          end
-        elsif time_decay_manner == "linear"
-          define_method("time_decay") do |initial_value, elapsed_time, decay_constant|
-            Decays.linear_decay(initial_value, elapsed_time, @time_decay_constant)
-          end
+
+        case time_decay_manner
+        when /exponential/
+          @time_decay_type = :exponential
+        when /linear/
+          @time_decay_type = :linear
         else
-          raise 'Time decay #{time_decay_manner} manner not recognized'
+          raise 'Time decay type #{time_decay_manner} not recognized!'
         end
+
         if distance_decay_manner == "exponential"
           define_method("distance_decay") do |initial_value, elapsed_time|
             Decays.exponential_decay(initial_value, elapsed_time, @distance_decay_constant)
@@ -34,11 +32,15 @@ module SPF
           raise 'Distance decay #{distance_decay_manner} manner not recognized'
         end
       end
-      
+
       def time_decay(initial_value, elapsed_time)
-         raise 'Method time_decay not implemented'
+        if @time_decay_type == :exponential
+          exponential_decay(initial_value, elapsed_time, @time_decay_constant)
+        else # :linear
+          linear_decay(initial_value, elapsed_time, @time_decay_constant)
+        end
       end
-      
+
       def distance_decay(initial_value, elapsed_time)
          raise 'Method distance_decay not implemented'
       end
@@ -46,6 +48,18 @@ module SPF
       def disseminate
         # TODO: implement
       end
+
+      private
+
+        def exponential_decay(initial_value, elapsed_time, exponential_decay_constant)
+          initial_value * Math.exp(-exponential_decay_constant * elapsed_time)
+        end
+
+        def linear_decay(initial_value, elapsed_time, linear_decay_constant)
+          result = initial_value - (elapsed_time * linear_decay_constant)
+          return result if result > 0
+          0
+        end
     end
   end
 end
