0a6b87226b1f16d62ce11d286a681e82e2e2edb9
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-08-17 10:37:33 +0200

    Implemented automatic unregistration of services in ServiceManager.

--- lib/spf/gateway/service_manager.rb
+++ lib/spf/gateway/service_manager.rb
@@ -15,7 +15,7 @@ module SPF
       # @param svc [SPF::Gateway::Service] the service to register
       def register_service(svc) # max_time?
         # add service to the set of services of corresponing type
-        (@services[svc.svc_type] ||= Set.new) << svc
+        (@services[svc.type] ||= Set.new) << svc
 
         # if a service has a maximum instantiation time, schedule its
         # deinstantiation
@@ -33,7 +33,7 @@ module SPF
           case svc.pipeline
           when :ocr
             # activate OCR pipeline
-            @pipelines[:ocr] = {
+            @active_pipelines[:ocr] = {
               pipeline: OCRPipeline.new,
               related_services: Set.new[ svc ]
             }
@@ -46,23 +46,36 @@ module SPF
       end
 
       # # do we actually need this method? isn't get_services_of_type enough?
-      # def has_services_of_type?(svc_type)
-      #   @services.has_key?(svc_type) and !@services[svc_type].empty?
+      # def has_services_of_type?(type)
+      #   @services.has_key?(type) and !@services[type].empty?
       # end
 
       # Returns all the registered services of a given type.
       #
       # @param [Symbol] The type of service the caller is interested in.
       # @return [Set] The set of services of requested type (or nil if empty).
-      def get_services_of_type(svc_type)
-        @services[svc_type]
+      def get_services_of_type(type)
+        @services[type]
       end
 
       private
 
-        def unregister_service(service)
-          # this is going to be called by a block of code inside a timer
-          # TODO
+        def unregister_service(svc)
+          # this method is going to be called by a block of code inside a timer
+
+          # find pipeline
+          pl = @active_pipelines[svc.pipeline]
+
+          # raise error if pipeline state is inconsistent
+          raise "Inconsistent state in ServiceManager!" unless pl
+
+          # remove service from pipeline
+          pl[:related_services].delete(svc)
+
+          # deactivate pipeline if needed
+          if pl[:related_services].empty?
+            pl[:pipeline].deactivate
+          end
         end
     end
 
