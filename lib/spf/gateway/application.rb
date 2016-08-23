03060c7938c28a477979fed9c2c5396ab564b141
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-08-23 11:59:40 +0200

    Application: added name parameter, fixed service creation, improved documentation.

--- lib/spf/gateway/application.rb
+++ lib/spf/gateway/application.rb
@@ -2,29 +2,30 @@ module SPF
   module Gateway
     class Application
 
+      attr_reader :name
       attr_reader :priority
 
       # Create application.
       #
+      # @param name [String] The application name.
       # @param config [Hash] The application configuration.
       # @param service_manager [SPF::Gateway::ServiceManager] The PIG ServiceManager instance.
-      def initialize(config, service_manager)
+      def initialize(name, config, service_manager)
+        @name = name.to_sym
         @priority = config[:priority]
 
-        # @services = Hash[
-        #   config[:service_policies].map do |service_name,service_conf|
-        #     [ service_name, Service.new(service_name, service_conf, service_registry) ]
-        #   end
-        # ]
-
-        @services = {}
-        config[:service_policies].each do |service_name,service_conf|
-          @services[service_name] = Service.new(service_name, service_conf, self, service_manager)
-        end
+        @services = Hash[
+          config[:service_policies].map do |service_name,service_conf|
+            [ service_name.to_sym, service_manager.instantiate_service(service_name.to_sym, service_conf, self) ]
+          end
+        ]
       end
 
       # Disseminate the processed results.
-      def disseminate
+      #
+      # @param io [Array] The IO to disseminate.
+      # @param voi [Float] VoI parameter (between 0.0 and 100.0) for the IO to disseminate.
+      def disseminate(io, voi)
         # TODO: implement
       end
 
