2c7e01d84f8539c023af90e2af5e968bdcd4ca55
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-08-23 12:18:52 +0200

    Major overhaul of Service and ServiceManager following the redesign of pipelines.

--- lib/spf/gateway/service.rb
+++ lib/spf/gateway/service.rb
@@ -1,131 +1,51 @@
+require 'forwardable'
+require_relative './gps'
+
 module SPF
   module Gateway
     class Service
-      attr_reader :type
+
+      # Dissemination is handled at the application level.
+      extend Forwardable
+      def_delegator :@application, :disseminate
+
+      attr_reader :name, :tau
 
       # Create service.
       #
       # @param name [String] The service name.
       # @param configuration [Hash] The service configuration.
       # @param application [SPF::Gateway::Application] The application this service refers to.
+      # @param service_strategy_class [Constant] The class of the service strategy to use.
       # @param service_manager [SPF::Gateway::ServiceManager] The PIG ServiceManager instance.
-      def initialize(name, configuration, application, service_manager)
+      def initialize(name, configuration, application, 
+                     service_strategy_class, service_manager)
+        @name = name.to_sym
+        @tau = configuration[:tau]
+        # TODO: should we use a factory here instead?
+        @service_strategy = service_strategy_class.new(
+          configuration[:time_decay],
+          configuration[:distance_decay])
         @application = application
         @service_manager = service_manager
-        @type = type
-
-        if configuration[:time_decay]
-          @time_decay_rules = configuration[:time_decay].dup.freeze
-        end
-
-        if configuration[:distance_decay]
-          @distance_decay_rules = configuration[:distance_decay].dup.freeze
-        end
-
-        #@closest_requestor_location = nil
-        @most_recent_request = nil
-        #@requestors = Set.new # TODO: check
-        #@max_number_of_requestors = 0
-        @requests = {}
       end
 
-      def time_decay(initial_value, elapsed_time)
-        if @time_decay_type == :exponential
-          exponential_decay(initial_value, elapsed_time, @time_decay_constant)
-        else # :linear
-          linear_decay(initial_value, elapsed_time, @time_decay_constant)
+      def register_request(socket)
+        while line = socket.gets do
+          req_id, req_loc, req_string = line.split(";")
+          @service_strategy.add_request(req_id, req_loc, req_string)
         end
       end
 
-      def distance_decay(initial_value, elapsed_time)
-        if @distance_decay_type == :exponential
-          exponential_decay(initial_value, elapsed_time, @time_decay_constant)
-        else # :linear
-          linear_decay(initial_value, elapsed_time, @distance_decay_constant)
-        end
-      end
-
-      def register_request(header, socket)
-        # TODO: update closest_recipient
-        #req.users.each do |user|
-          #@requestors.add(user)
-        #end
-
-        # update @max_number_of_requestors
-        #if @requestors.size > @max_number_of_requestors
-         # @max_number_of_requestors = @requestors.size
-        #end
-
-        # TODO: update @closest_requestor_location
-        #@closest_requestor_location = ...
-
-        @requests[header[2]] = socket
-
-        # update @most_recent_request
-        @most_recent_request = header
-      end
-
-      def unregister_request(header)
-        # TODO: update closest_recipient
-        #req.users.each do |user|
-          #@requestors.delete(req.user)
-        #end
-
-        #delete request and close socket if open
-        @requests.delete(header[2]).close
-
-        # TODO: update @most_recent_request
-
-        # TODO: update @closest_requestor_location
-      end
-
-      def new_data(raw_data)
-        # do not process unless we have requestors
-        return unless @requests.size
-
-        # do the actual processing
-        io = @processing_pipeline.process(raw_data) # services might share pipelines
-
-        # if raw_data was not different enough from previously processed
-        # raw_data, it won't be processed
-        return unless io
-
-        # calculate voi
-        #voi = calculate_max_voi(io)
+      def new_information(io, source)
+        # get response from service strategy
+        response, voi =
+          @service_strategy.execute_service(io, source)
 
         # disseminate calls DisService
-        #@app.disseminate(io, voi)
-        @requests.each_pair {|req_string, socket| socket.write(io) if io.include? req_string}
+        @app.disseminate(response, voi)
       end
 
-      def calculate_max_voi
-        # VoI(o,r,t,a)= PA(a) * RN(r) * TRD(t,OT(o)) * PRD(OL(r),OL(o))
-        p_a = @app.priority
-        r_n = @requestors.size / @max_number_of_requestors
-        t_rd = apply_decay(Time.now - @most_recent_request.time, @time_decay_rules)
-        p_rd = apply_decay(GPS.calculate_distance(PIG.location, @closest_requestor_location), @distance_decay)
-        p_a * r_n * t_rd * p_rd
-      end
-
-      private
-
-        def apply_decay(value, rules)
-          # enforce maximum value if needed
-          return 0.0 if rules[:max] and value > rules[:max]
-
-          # apply decay according to the requested type
-          decay_modifier = case rules[:type]
-          when :exponential
-            Math.exp(-value)
-          when :linear
-            1.0 - value / rules[:max].to_f
-          else
-            1.0 # default is no decay at all
-          end
-
-          # apply decay modifier to value
-          value * decay_modifier
-        end
     end
   end
 end
--- lib/spf/gateway/service_manager.rb
+++ lib/spf/gateway/service_manager.rb
@@ -4,64 +4,90 @@ module SPF
   module Gateway
 
     class ServiceManager
+      # Initializes the service manager.
       def initialize
         @services = {}
         @active_pipelines = {}
         @timers = Timers::Group.new
       end
 
-      # Registers service
+      # Instantiates (creates and activates) a service.
       #
-      # @param svc [SPF::Gateway::Service] the service to register
-      def register_service(svc) # max_time?
-        # add service to the set of services of corresponing type
-        (@services[svc.type] ||= Set.new) << svc
+      # @param service_name [String] Name of the service to instantiate.
+      # @param config [String] Configuration of the service to instantiate.
+      # @param application [SPF::Gateway::Application] The application the service to instantiate belongs to.
+      def instantiate_service(service_name, config, application)
+        # create service...
+        svc = Service.new(service_name, config, application, self)
 
-        # if a service has a maximum instantiation time, schedule its
-        # deinstantiation
-        if svc.max_time
-          @timers.after(svc.max_time) { unregister_service(svc) }
-        end
+        # add service to the set of services of corresponing application
+        @services[application.name] ||= {}
+        # TODO: we operate under the assumption that the (application_name,
+        # service_name) couple is unique for each service. Make sure the
+        # assumption holds, so that the following statement does not overwrite
+        # anything!!!
+        @services[application.name][service_name] = svc
 
-        # instantiate pipeline if needed
-        pipeline = @active_pipelines[svc.pipeline]
-        if pipeline
-          unless pipeline[:related_services].includes? svc
-            pipeline[:related_services] << svc
-          end
-        else
-          case svc.pipeline
-          when :ocr
-            # activate OCR pipeline
-            @active_pipelines[:ocr] = {
-              pipeline: OCRPipeline.new,
-              related_services: Set.new[ svc ]
-            }
-          when :audio
-            # TODO
-          when :object_count
-            # TODO
-          end
-        end
+        # ...and activate it
+        activate_service(svc)
       end
 
-      # # do we actually need this method? isn't get_services_of_type enough?
-      # def has_services_of_type?(type)
-      #   @services.has_key?(type) and !@services[type].empty?
-      # end
+      def get_service_by_name(application_name, service_name)
+        # TODO: we operate under the assumption that the (application_name,
+        # service_name) couple is unique for each service. Make sure the
+        # assumption holds, so that the following statement returns just one service.
+        @services[application_name][service_name]
+      end
 
-      # Returns all the registered services of a given type.
-      #
-      # @param [Symbol] The type of service the caller is interested in.
-      # @return [Set] The set of services of requested type (or nil if empty).
-      def get_services_of_type(type)
-        @services[type]
+      def with_pipelines_interested_in(raw_data)
+        interested_pipelines = @active_pipelines.select {|p| p.interested_in?(raw_data) }
+
+        interested_pipelines.each do |p|
+          yield p
+        end
       end
 
       private
 
-        def unregister_service(svc)
+        # Activates a service
+        #
+        # @param svc [SPF::Gateway::Service] the service to activate.
+        def activate_service(svc) # max_time?
+          # if a service has a maximum instantiation time, schedule its
+          # deinstantiation
+          if svc.max_time
+            @timers.after(svc.max_time) { deactivate_service(svc) }
+          end
+
+          # instantiate pipeline if needed
+          pipeline = @active_pipelines[svc.pipeline]
+          unless pipeline
+            pipeline = case svc.pipeline
+            when :ocr
+              # TODO: pass as parameter what the pipeline should look for?
+              # activate OCR pipeline
+              @active_pipelines[:ocr] =
+                Pipeline.new(svc.tau, OCRProcessingStrategy.new("water"))
+            when :audio
+              # TODO
+            when :object_count
+              # TODO
+            else
+              raise "Unknown pipeline"
+            end
+          end
+
+          pipeline.register_service(svc)
+        end
+
+        # Deactivates a service
+        #
+        # @param svc [SPF::Gateway::Service] The service to deactivate.
+        def deactivate_service(svc)
           # this method is going to be called by a block of code inside a timer
+
+          # remove service
+          @services[svc.application.name].delete(svc.name)
 
           # find pipeline
           pl = @active_pipelines[svc.pipeline]
