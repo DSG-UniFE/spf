a8dbeaeac7640e5a1749d31ae93b4a794704f401
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-08-23 12:26:00 +0200

    Fixed DataListener, ConfigurationAgent, and ServiceManager instantiation.

--- lib/spf/pig.rb
+++ lib/spf/pig.rb
@@ -5,22 +5,26 @@ module SPF
     DEFAULT_IOT_PORT = 2160
     DEFAULT_PROGRAMMING_PORT = 52160
 
+    attr_reader :location
+
     def initialize(host,
                    conf_filename,
                    iot_address = 'localhost',
                    iot_port = DEFAULT_IOT_PORT,
                    programming_address = 'localhost',
                    programming_port = DEFAULT_PROGRAMMING_PORT)
+      config = PIGConfiguration::load_from_file(conf_filename) 
       @iot_address         = iot_address
       @iot_port            = iot_port
       @programming_address = programming_address
       @programming_port    = programming_port
-      @config              = Configuration::load_from_file(conf_filename) 
+      @location            = config[:location]
+      @service_manager     = ServiceManager.new
     end
 
     def run
-      Thread.new { SPF::Gateway::Processor.new(@iot_address, @iot_port, @config).run }
-      Thread.new { SPF::Gateway::Controller.new(@programming_address, @programming_port).run }
+      Thread.new { SPF::Gateway::DataListener.new(@iot_address, @iot_port, @service_manager).run }
+      Thread.new { SPF::Gateway::ConfigurationAgent.new(@programming_address, @programming_port).run }
     end
   end
 end
