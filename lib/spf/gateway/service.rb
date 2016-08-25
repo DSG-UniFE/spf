18b555bbbcd8deaf9c8d57e20fe91155c5b9ece2
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-08-25 12:29:01 +0200

    Initialize method now receives an instance of a service strategy as a parameter register request modified to comply with the format of request messages

--- lib/spf/gateway/service.rb
+++ lib/spf/gateway/service.rb
@@ -18,21 +18,25 @@ module SPF
       # @param application [SPF::Gateway::Application] The application this service refers to.
       # @param service_strategy_class [Constant] The class of the service strategy to use.
       # @param service_manager [SPF::Gateway::ServiceManager] The PIG ServiceManager instance.
-      def initialize(name, configuration, application, 
-                     service_strategy_class, service_manager)
+      def initialize(name, service_conf, application, 
+                     service_strategy, service_manager)
         @name = name.to_sym
-        @tau = configuration[:tau]
+        @tau = service_conf[:tau]
         # TODO: should we use a factory here instead?
-        @service_strategy = service_strategy_class.new(
-          configuration[:time_decay],
-          configuration[:distance_decay])
+        @service_strategy = service_strategy
         @application = application
         @service_manager = service_manager
       end
 
+      # 001;11.48,45.32\n
+      # find "water"\n
+      # 002;11.48,45.32\n
+      # find "food"\n
       def register_request(socket)
         while line = socket.gets do
-          req_id, req_loc, req_string = line.split(";")
+          req_id, req_loc = line.split(";")
+          req_string = socket.gets
+          raise "Wrong request format" if req_string.nil?
           @service_strategy.add_request(req_id, req_loc, req_string)
         end
       end
