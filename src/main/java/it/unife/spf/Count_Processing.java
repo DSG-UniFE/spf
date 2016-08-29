8915be9700fec743accaa89fecac75e271f79081
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-08-29 17:06:53 +0200

    More code cleanup

--- src/main/java/it/unife/spf/Count_Processing.java
+++ src/main/java/it/unife/spf/Count_Processing.java
@@ -8,55 +8,43 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 
-import org.opencv.imgproc.Imgproc; // for opencv3.0.0
+import org.opencv.imgproc.Imgproc; 				// for opencv3.0.0
 import org.opencv.objdetect.CascadeClassifier;
-//import org.opencv.highgui.Highgui; // for opencv2.4.11
-import org.opencv.imgcodecs.Imgcodecs; //for opencv3.0.0
+import org.opencv.imgcodecs.Imgcodecs; 			// for opencv3.0.0
 
 public class Count_Processing {
 
-
 	static {
-
 		System.loadLibrary("opencv_java300");
-
 	}
 	
-	public static String CountObject(String file){
-		
-		
-		 try{
-	        	
-	        	
-	            CascadeClassifier objDetector = new CascadeClassifier("cars.xml");
-	            objDetector.load("cars.xml");
-	            File f = new File(file);
-	           
-	            	
-	            	String name = f.getName();
-	            	System.out.println(name);
-	            
-	            Mat image = Imgcodecs.imread(name);
-	            MatOfRect objDetections = new MatOfRect();
-	            objDetector.detectMultiScale(image, objDetections);
-	            objDetector.detectMultiScale(image, objDetections, 1.2, 3, 0, new Size(),new Size());
-	            for (Rect rect : objDetections.toArray()) {
-	            	if (rect.x>50 && rect.y>50)
-		             Imgproc.rectangle(image, new Point(rect.x, rect.y),new Point(rect.x + rect.width, rect.y + rect.height),new Scalar( 0, 0, 255),4);
-		         }
-	            Imgcodecs.imwrite("recognized-"+name,image);
-	            
-	            image.release();
-	            return ""+objDetections.toArray().length;
-	            
-	        }
-	        
-	        catch(Exception e)
-	        {
-	            System.out.println("Eccezione Core Processing: "+e);
-	            return "";
-	        }
-	        
+	public static String CountObject(String file) {
+		try {
+			CascadeClassifier objDetector = new CascadeClassifier("cars.xml");
+			objDetector.load("cars.xml");
+			File f = new File(file);
+
+			String name = f.getName();
+			System.out.println(name);
+
+			Mat image = Imgcodecs.imread(name);
+			MatOfRect objDetections = new MatOfRect();
+			objDetector.detectMultiScale(image, objDetections);
+			objDetector.detectMultiScale(image, objDetections, 1.2, 3, 0, new Size(), new Size());
+			for (Rect rect : objDetections.toArray()) {
+				if (rect.x > 50 && rect.y > 50)
+					Imgproc.rectangle(image, new Point(rect.x, rect.y),
+							new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 4);
+			}
+			Imgcodecs.imwrite("recognized-" + name, image);
+
+			image.release();
+			return "" + objDetections.toArray().length;
+		}
+		catch (Exception e) {
+			System.out.println("Eccezione Core Processing: " + e);
+			return "";
+		}
 	}
 	
 }
--- src/main/java/it/unife/spf/TextRecognition.java
+++ src/main/java/it/unife/spf/TextRecognition.java
@@ -17,58 +17,50 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.opencv.core.Core;
-import org.opencv.imgproc.Imgproc; // for opencv3.0.0
-
-
-//import org.opencv.highgui.Highgui; // for opencv2.4.11
-import org.opencv.imgcodecs.Imgcodecs; //for opencv3.0.0
+import org.opencv.imgproc.Imgproc; 		// for opencv3.0.0
+import org.opencv.imgcodecs.Imgcodecs; 	// for opencv3.0.0
 
 
 public class TextRecognition {
-
 
 	static {
 		System.loadLibrary("opencv_java300");
 	}
 	
 	public static String doOCR(String file) {
-		Mat source = Imgcodecs.imread(file,0);
-		//Imgcodecs.imwrite("img-water-grayscale.jpg", source);
-		//Imgproc.cvtColor(source, source, Imgproc.COLOR_BGR2GRAY);
-		Mat destination = new Mat(source.rows(),source.cols(),source.type());
-		Imgproc.threshold(source,destination, 0, 255 ,Imgproc.THRESH_OTSU);
-		//Imgproc.adaptiveThreshold(destination, new Mat(), 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-	       
+		Mat source = Imgcodecs.imread(file, 0);
+		Mat destination = new Mat(source.rows(), source.cols(), source.type());
+		Imgproc.threshold(source, destination, 0, 255, Imgproc.THRESH_OTSU);
+
 		Imgcodecs.imwrite("text-recognition-threshold.jpg", destination);
 		source.release();
 		int white = Core.countNonZero(destination);
 		int black = (int) (destination.size().area() - white);
-		
+
 		if (white > black) {
 			Core.bitwise_not(destination, destination);
 		}
 		Imgcodecs.imwrite("text-recognition-treshZero.jpg", destination);
-		
-		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS,new Size(3,3));
+
+		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS, new Size(3, 3));
 		Mat dilated = new Mat();
-		Imgproc.dilate(destination, dilated, kernel, new Point(-1,-1), 30);
-		
+		Imgproc.dilate(destination, dilated, kernel, new Point(-1, -1), 30);
+
 		destination.release();
 		kernel.release();
 		Imgcodecs.imwrite("text-recognition-dilated.jpg", dilated);
-		
+
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		Mat hierarchy = new Mat();
-		Imgproc.findContours(dilated, contours,hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-		
+		Imgproc.findContours(dilated, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
 		for (int i = 0; i < contours.size(); i++) {
-			Imgproc.drawContours(dilated, contours, i, new Scalar(255,0,0), 2, 8, hierarchy, 0, new Point());
-	    }
+			Imgproc.drawContours(dilated, contours, i, new Scalar(255, 0, 0), 2, 8, hierarchy, 0, new Point());
+		}
 		hierarchy.release();
 		Imgcodecs.imwrite("text-recognition-contours.jpg", dilated);
 		dilated.release();
-		//System.out.println(contours.size());
-		
+
 		ArrayList<RotatedRect> areas = new ArrayList<RotatedRect>();
 		for (MatOfPoint contour : contours) {
 			// Find it's rotated rect
@@ -82,7 +74,8 @@ public class TextRecognition {
 
 			if (box.angle < -45.0) {
 				proportion = box.size.height / box.size.width;
-			} else {
+			}
+			else {
 				proportion = box.size.width / box.size.height;
 			}
 
@@ -92,74 +85,73 @@ public class TextRecognition {
 
 			areas.add(box);
 		}
-		//System.out.println(areas.size());
-		 
-		 
+
 		int i = 1;
-		String response="";
+		String response = "";
 		System.out.println(areas.size());
 		for (RotatedRect rect : areas) {
-			Mat img = Imgcodecs.imread(file,0);
-			//System.out.println(rect.angle);
+			Mat img = Imgcodecs.imread(file, 0);
 			Rect brect = rect.boundingRect();
-			
-			Imgproc.rectangle(img, new Point(brect.x, brect.y), new Point(brect.x + brect.width, brect.y + brect.height),  new Scalar(110, 220, 0), 5);
+
+			Imgproc.rectangle(img, new Point(brect.x, brect.y),
+					new Point(brect.x + brect.width, brect.y + brect.height), new Scalar(110, 220, 0), 5);
 			System.out.println(i);
 			System.out.println(img.height());
 			System.out.println(img.width());
-			
+
 			if (brect.x < 0 || brect.y < 0) {
 				continue;
 			}
-			
+
 			Mat a = img.submat(brect);
 			System.out.println("submat");
-			Imgcodecs.imwrite("part-"+i+".jpg", a);
-			
-			Mat sub_image = Imgcodecs.imread("part-"+i+".jpg",0);
+			Imgcodecs.imwrite("part-" + i + ".jpg", a);
+
+			Mat sub_image = Imgcodecs.imread("part-" + i + ".jpg", 0);
 			Mat adaptive = new Mat();
-	        Imgproc.adaptiveThreshold(sub_image, adaptive, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-	        executeCommand("tesseract "+"part-"+i+".jpg"+" output-"+"part-"+i+" "+"-l eng -psm 8");
-			// PSM value from tesseract manual:  8 = Treat the image as a single word.
-	        Path part = Paths.get("./", "output-"+"part-"+i+".txt");
-	        try {
+			Imgproc.adaptiveThreshold(sub_image, adaptive, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
+					Imgproc.THRESH_BINARY, 11, 2);
+			// PSM value from tesseract manual: 8 = Treat the image as a single word.
+			executeCommand("tesseract " + "part-" + i + ".jpg" + " output-" + "part-" + i + " " + "-l eng -psm 8");
+			
+			Path part = Paths.get("./", "output-" + "part-" + i + ".txt");
+			try {
 				String t = new String(Files.readAllBytes(part));
 				System.out.println(t);
-				response+=t+" ";
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();	
+				response += t + " ";
 			}
-		  	i++;
-		  	sub_image.release();
-		  	adaptive.release();
-		  	img.release();
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			i++;
+			sub_image.release();
+			adaptive.release();
+			img.release();
 		}
 		
-		//FaceRecognition.doFaceRec("street-crowd.jpg");
-		//Imgcodecs.imwrite("text-recognition-dilated-block-segmentation.jpg", img);
 		System.out.println(response);
 		return response;
 	}
-	
+
 	public static String executeCommand(String command) {
 		StringBuffer output = new StringBuffer();
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
 			p.waitFor();
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-                        String line = "";			
-			while ((line = reader.readLine())!= null) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
 				output.append(line + "\n");
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return output.toString();
 	}
-	
+
 }
