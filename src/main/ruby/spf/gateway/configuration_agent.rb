c4e1ff85ffd847f0afca3d26ae447dbf6cdfadb1
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-08-30 15:02:09 +0200

    updated new_service_request method to comply with the change of interface in the Service and ServiceManager classes

--- src/main/ruby/spf/gateway/configuration_agent.rb
+++ src/main/ruby/spf/gateway/configuration_agent.rb
@@ -107,11 +107,12 @@ module SPF
         def new_service_request(application_name, service_name, socket)
           # find service
           svc = @service_manager.get_service_by_name(application_name, service_name)
+          return if svc.nil?
           
           # bring service up again if down
-          @service_manager.instantiate_service() if svc.nil?
+          @service_manager.restart_service(svc) if !svc.active?
           
-          # reset service timer
+          # reset service timer following the new request
           @service_manager.reset_timer(svc)
 
           # update service
