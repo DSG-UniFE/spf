ba6dfea86be8a8bb4795d7d17518e6801676297b
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-09-07 15:21:28 +0200

    Updated implementation of the disseminate method

--- gateway-module/src/main/ruby/spf/gateway/application.rb
+++ gateway-module/src/main/ruby/spf/gateway/application.rb
@@ -1,6 +1,8 @@
 module SPF
   module Gateway
     class Application
+      
+      DEFAULT_RESPONSE_EXPIRATION_TIME = 2 * 60 * 1000    # 2 minutes
 
       attr_reader :name
       attr_reader :priority
@@ -10,9 +12,12 @@ module SPF
       # @param name [String] The application name.
       # @param config [Hash] The application configuration.
       # @param service_manager [SPF::Gateway::ServiceManager] The PIG ServiceManager instance.
-      def initialize(name, config, service_manager)
+      # @param disservice_handler [SPF::Gateway::DisServiceHandler] The DisServiceHandler instance.
+      def initialize(name, config, service_manager, disservice_handler)
         @name = name.to_sym
         @priority = config[:priority]
+        @response_expiration_time = DEFAULT_RESPONSE_EXPIRATION_TIME
+        @disservice_handler = disservice_handler
 
         @services = Hash[
           config[:service_policies].map do |service_name,service_conf|
@@ -21,14 +26,15 @@ module SPF
         ]
       end
 
+
       # Disseminate the processed results.
       #
       # @param io [Array] The IO to disseminate.
       # @param voi [Float] VoI parameter (between 0.0 and 100.0) for the IO to disseminate.
-      def disseminate(io, voi)
-        # TODO: implement
+      def disseminate(mime_type, io, voi)
+        @disservice_handler.push(@name.to_s, "", "", mime_type, io, voi, response_expiration_time)
       end
-
+      
     end
   end
 end
