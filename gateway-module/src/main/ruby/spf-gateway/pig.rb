92371541719d08308ebc43c4d64b255ecb63db5e
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-11-24 17:41:15 +0100

    Improved design of PIG class.

--- gateway-module/src/main/ruby/spf-gateway/pig.rb
+++ gateway-module/src/main/ruby/spf-gateway/pig.rb
@@ -1,31 +1,37 @@
-require 'spf/gateway/controller'
+require 'forwardable'
+
+require 'spf-gateway/controller'
 
 module SPF
-  class PIG
-    DEFAULT_IOT_PORT = 2160
-    DEFAULT_PROGRAMMING_PORT = 52160
+  module Gateway
+    class PIG
+      DEFAULT_IOT_PORT = 2160
+      DEFAULT_PROGRAMMING_PORT = 52160
 
-    attr_reader :location
+      # delegate location to @config
+      extend Forwardable
+      def_delegator :@config, :location
 
-    def initialize(host,
-                   conf_filename,
-                   iot_address = 'localhost',
-                   iot_port = DEFAULT_IOT_PORT,
-                   programming_address = 'localhost',
-                   programming_port = DEFAULT_PROGRAMMING_PORT)
-      config = PIGConfiguration::load_from_file(conf_filename)
-      @iot_address         = iot_address
-      @iot_port            = iot_port
-      @programming_address = programming_address
-      @programming_port    = programming_port
-      @location            = config[:location]
-      @service_manager     = ServiceManager.instance
-      @disservice_handler  = DisServiceHandler.new
-    end
+      def initialize(configuration,      # PIGConfiguration::load_from_file(conf_filename)
+                     service_manager,    # ServiceManager.instance
+                     disservice_handler, # DisServiceHandler.new
+                     iot_address = 'localhost',
+                     iot_port = DEFAULT_IOT_PORT,
+                     programming_address = 'localhost',
+                     programming_port = DEFAULT_PROGRAMMING_PORT)
+        @config              = configuration
+        @service_manager     = service_manager
+        @disservice_handler  = disservice_handler
+        @iot_address         = iot_address
+        @iot_port            = iot_port
+        @programming_address = programming_address
+        @programming_port    = programming_port
+      end
 
-    def run
-      Thread.new { SPF::Gateway::DataListener.new(@iot_address, @iot_port, @service_manager).run }
-      Thread.new { SPF::Gateway::ConfigurationAgent.new(@programming_address, @programming_port).run }
+      def run
+        Thread.new { SPF::Gateway::DataListener.new(@iot_address, @iot_port, @service_manager).run }
+        Thread.new { SPF::Gateway::ConfigurationAgent.new(@programming_address, @programming_port).run }
+      end
     end
   end
 end
