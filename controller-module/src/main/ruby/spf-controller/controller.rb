3e0b72d69707d0a6265506261780681ce34f122a
Alexdp74 <alessandro.morelli.1987@gmail.com>
2016-11-25 12:10:16 +0100

    Controller now uses a kd-tree to control PIGs locations; added code to connect to all PIGs at startup

--- controller-module/src/main/ruby/spf-controller/controller.rb
+++ controller-module/src/main/ruby/spf-controller/controller.rb
@@ -1,15 +1,28 @@
 require 'spf-common/controller'
+require 'spf-common/logger'
+require 'spf-common/validate'
+require 'geokdtree'
+
 
 module SPF
+  
+  include SPF::Logging
+  
   class Controller < SPF::Common::Controller
     
     def initialize(host, port)
       config = PIGConfiguration::load_from_file(conf_filename)
-      @pigs = config.pigs
-      @pig_connections = {}
-      @reconf_template = read_reconf_template(template_filename)
+      @pigs_list = config.pigs
       
+      @pigs_tree = Geokdtree::Tree.new(2)
+      @pigs_list.each do |pig|
+        @pigs_tree.insert([pig['gps_lat'], pig['gps_lon']], pig)
+      end
+      
+      @pig_connections = {}
       connect_to_pigs
+      
+      @reconf_template = read_reconf_template(template_filename)
       
       super(host, port)
     end
@@ -45,9 +58,19 @@ module SPF
         user_socket.close
         
         # get gps coords
-        lat, long = extract_gps_from_request_body(body)
+        _, lat, lon, _ = parse_request_body(body)
+        unless SPF::Validate.latitude?(lat) && SPF::Validate.longitude?(lon)
+          logger.error "Error in client GPS coordinates"
+          return
+        end
         
-        pig = find_nearest_pig(lat, long)
+        result = @pigs_tree.nearest([lat.to_f, lon.to_f])
+        if result.nil?
+          logger.fatal "Could not find the nearest PIG (empty data structure?)"
+          return
+        end
+        
+        pig = result.data.inspect
         pig_socket = @pig_connections[(pig.ip + ":" + pig.port).to_sym]      # check
         if pig_socket.nil? or pig_socket.closed?
           pig_socket = TCPSocket.new(pig.ip, pig.port)
@@ -64,11 +87,19 @@ module SPF
 
       # Open socket to all pigs in the @pigs list
       def connect_to_pigs
-        
+        @pigs_list.each do |pig|
+          pig_socket = TCPSocket.new(pig.ip, pig.port)
+          @pig_connections[(pig.ip + ":" + pig.port).to_sym] = pig_socket
+        end
       end
       
       def read_reconf_template(template_filename)
         @reconf_template = File.new(template_filename, 'r').read
+      end
+      
+      def parse_request_body(body)
+        tmp = body.split(';')
+        [tmp[0]] + tmp[1].split(',') + [tmp[2]]
       end
       
   end
