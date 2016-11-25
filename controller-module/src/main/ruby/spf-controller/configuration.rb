36e3ac71ea2434aa2494f5f8d37be2d996b78c0f
giulio <giulio.ribe@gmail.com>
2016-11-25 14:35:31 +0100

    Updated controller's configuration class

--- controller-module/src/main/ruby/spf-controller/configuration.rb
+++ controller-module/src/main/ruby/spf-controller/configuration.rb
@@ -1,39 +1,47 @@
 require_relative './service_manager'
 require_relative './application'
 require 'pig_details'
+require 'spf-common/validate'
+require 'spf-common/logger'
 
 module SPF
   module Controller
 
     class Configuration
+      include SPF::Logging
 
       attr_reader :pigs
-      
+
       private
-      
+
         def initialize
           @pigs = []
         end
-  
+
         def validate
           # check coordinates are valid, IPs are valid, ports are valid
+          @pigs.delete_if {|pig| SPF::Validate.pig? pig }
         end
-        
+
+      def add_pigs(pigs)
+        @pigs = pigs
+      end
+
       public
-          
+
         def self.load_from_file(filename)
           # allow filename, string, and IO objects as input
           raise ArgumentError, "File #{filename} does not exist!" unless File.exists?(filename)
-  
+
           # create configuration object
           conf = Configuration.new
-  
+
           # take the file content and pass it to instance_eval
-          @pigs = conf.instance_eval(File.new(filename, 'r').read)
-  
+          conf.instance_eval(File.new(filename, 'r').read)
+
           # validate and finalize configuration
           conf.validate
-  
+
           # return new object
           conf
         end
