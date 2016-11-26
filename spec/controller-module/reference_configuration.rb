986ab164b66150a25ec02168446940c00cfd98e5
giulio <giulio.ribe@gmail.com>
2016-11-26 11:25:49 +0100

    Added application configuration

--- spec/controller-module/reference_configuration.rb
+++ spec/controller-module/reference_configuration.rb
@@ -1,6 +1,40 @@
 require 'spf-controller/configuration'
 require 'spf-common/extensions/fixnum'
 
+
+APPLICATION_CHARACTERIZATION = <<END
+application "participants",
+{
+  priority: 50.0,
+  allow_services: [ :find_text, :listen ],
+  service_policies: {
+    find_text: {
+      processing_pipeline: :ocr,
+      filtering_threshold: 0.05,
+      uninstall_after: 2.minutes,
+      distance_decay: {
+        type: :exponential,
+        max: 1.km
+      }
+    },
+    listen: {
+      processing_pipeline: :identify_song,
+      time_decay: {
+        type: :linear,
+        max: 2.minutes
+      }
+    }
+  },
+  dissemination_policy: {
+    subscription: "participants",
+    retries: 1,
+    wait: 30.seconds,
+    on_update: :overwrite,
+    allow_channels: :WiFi
+  }
+}
+END
+
 PIGS_CHARACTERIZATION = <<END
 add_pigs [
   {
@@ -22,6 +56,7 @@ END
 # this is the whole reference configuration
 # (useful for spec'ing configuration.rb)
 REFERENCE_CONFIGURATION =
+  APPLICATION_CHARACTERIZATION +
   PIGS_CHARACTERIZATION
 
 # evaluator = BasicObject.new
