80af19473ab81a5561fdca886cd8d0499aa844b2
Mauro Tortonesi <mauro.tortonesi@unife.it>
2016-12-16 12:23:22 +0100

    Load all jars before running tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,11 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 # Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
+
+if RUBY_PLATFORM =~ /java/
+  $CLASSPATH << '/usr/share/java/opencv.jar'
+  JARS_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', 'jars'))
+  Dir["#{JARS_DIR}/*.jar"].each do |jar|
+    $CLASSPATH << jar unless $CLASSPATH.include?(jar)
+  end
+end
