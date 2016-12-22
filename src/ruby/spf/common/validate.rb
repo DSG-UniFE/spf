cc11b49221726b273e2ee39ea47ce6bcfd668cd5
giulio <giulio.ribe@gmail.com>
2016-12-22 17:38:06 +0100

    Fixed validate class

--- src/ruby/spf/common/validate.rb
+++ src/ruby/spf/common/validate.rb
@@ -3,14 +3,12 @@ require 'resolv'
 module SPF
   module Common
     class Validate
-      
+
       @@KEYS = [:priority, :allow_services, :service_policies, :dissemination_policy]
-      @@TIMES = ["second", "seconds", "minute", "minutes", "hour", "hours", "day",
-        "days", "month", "months", "year", "years"]
       @@DISTANCE_TYPES = [:linear, :exponential]
       @@CHANNELS = [:WiFi, :cellular]
-      @@SERVICES_FOLDER = File.join('src', 'ruby', 'gateway', 'service-strategies')
-      @@PROCESS_FOLDER = File.join('src', 'ruby', 'gateway', 'processing-strategies')
+      @@SERVICES_FOLDER = File.join('src', 'ruby', 'spf', 'gateway', 'service-strategies')
+      @@PROCESS_FOLDER = File.join('src', 'ruby', 'spf', 'gateway', 'processing-strategies')
 
       def self.ip?(ip)
         ip =~ Regexp.union([Resolv::IPv4::Regex, Resolv::IPv6::Regex]) ? true : false
@@ -37,101 +35,66 @@ module SPF
       end
 
       def self.conf?(opt)
-        # check conf
-        # {
-        #   priority: 50.0, -> 0..100
-        #   allow_services: [ :find_text, :listen ], # controllare 'service-strategies'
-        #   service_policies: {
-        #     find_text: {
-        #       processing_pipeline: :ocr, -> 'processing-strategies'
-        #       filtering_threshold: 0.05, -> 0..1
-        #       uninstall_after: 2.minutes, -> >0
-        #       distance_decay: {
-        #         type: :exponential, -> linear or exp
-        #         max: 1.km -> >0
-        #       }
-        #     },
-        #     listen: {
-        #       processing_pipeline: :identify_song,
-        #       time_decay: {
-        #         type: :linear,
-        #         max: 2.minutes
-        #       }
-        #     }
-        #   },
-        #   dissemination_policy: {
-        #     subscription: "participants", -> stringa
-        #     retries: 1, -> =>0
-        #     wait: 30.seconds, -> =>0
-        #     on_update: :overwrite, -> ?
-        #     allow_channels: :WiFi -> wifi cellular
-        #   }
-        # }
+        return false unless (opt.keys & @@KEYS).any?
 
-        return false unless (opt.keys & @@KEYS).any? #and (opt.length == KEYS.length)
+        services = Dir.entries(@@SERVICES_FOLDER)
+        process = Dir.entries(@@PROCESS_FOLDER)
 
         opt.keys.each do |key|
-
           case key
 
           when :priority
             return false unless opt[key].between?(0, 100)
 
           when :allow_services
-            services = Dir.glob(File.join(@@SERVICES_FOLDER, "*"))
             opt[key].each do |service|
               return false unless services.include?(service.to_s + "_service_strategy.rb")
             end
 
           when :service_policies
-            opt[key][:service_policies].keys.each do | service |
+            opt[key].keys.each do | service |
               case service
+
               when :find_text
-                process = Dir.glob(File.join(@@PROCESS_FOLDER, "*"))
-                opt[key][:service_policies][:processing_pipeline].each do |pro|
-                  return false unless process.include?(pro.to_s + "_processing_strategy.rb")
-                end
-    
-                return false unless opt[key][:service_policies][:processing_pipeline][:filtering_threshold].between?(0, 1)
-    
-                uninstall_after = opt[key][:service_policies][:processing_pipeline][:uninstall_after]
-    
-                n_time, s_time = uninstall_after.split('.')
-                return false unless n_time >= 0
-                return false unless @@TIMES.include?(s_time)
-    
-                return false unless @@DISTANCE_TYPES.include?(opt[key][:service_policies][:processing_pipeline][:distance_decay][:type])
-                return false unless opt[key][:service_policies][:processing_pipeline][:distance_decay][:max] >= 0
-    
-              # TODO
-              when :listen
-                raise "Case :listen not implemented yet!"
-    
+                return false unless process.include?(opt[key][service][:processing_pipeline].to_s + "_processing_strategy.rb")
+
+                return false unless opt[key][service][:filtering_threshold].between?(0, 1)
+
+                return false unless opt[key][service][:uninstall_after] >= 0
+
+                return false unless @@DISTANCE_TYPES.include?(opt[key][service][:distance_decay][:type])
+                return false unless opt[key][service][:distance_decay][:max] >= 0
+
+              when :audio_info
+
+                # return false unless process.include?(opt[key][service][:processing_pipeline].to_s + "_processing_strategy.rb")
+
+                return false unless @@DISTANCE_TYPES.include?(opt[key][service][:time_decay][:type])
+                return false unless opt[key][service][:time_decay][:max] >= 0
               else
                 return false
-    
               end
+
             end
 
           when :dissemination_policy
-            return false unless opt[key][:dissemination_policy][:subscription].is_a? String
-            return false unless opt[key][:dissemination_policy][:retries] >= 0
-            return false unless opt[key][:dissemination_policy][:wait] >= 0
+            return false unless opt[key][:subscription].is_a? String
+            return false unless opt[key][:retries] >= 0
+            return false unless opt[key][:wait] >= 0
 
             # TODO
-            # return false unless opt[key][:dissemination_policy][:on_update]
+            # return false unless opt[key][service][:on_update]
 
-            return false unless @@CHANNELS.include?(opt[key][:dissemination_policy][:allow_channels])
+            return false unless @@CHANNELS.include?(opt[key][:allow_channels])
 
           else
             return false
-          
+
           end
 
         end
 
         return true
-
       end
 
     end
