88027f2c30610f59a9d413f6623ad6d9babc585f
giulio <giulio.ribe@gmail.com>
2016-12-28 11:55:14 +0100

    Added test for controller configuration

--- spec/controller/configuration_spec.rb
+++ spec/controller/configuration_spec.rb
@@ -9,8 +9,15 @@ describe SPF::Controller::Configuration do
 
   it 'should correctly detect number of pigs' do
     with_controller_reference_config do |conf|
-      conf.pigs.size.must_equal 2
+      conf.size.must_equal 2
     end
   end
 
+  it 'should correctly detect pig port' do
+    with_controller_reference_config do |conf|
+      conf.each do |pig|
+        pig[:port].must_equal 52160
+      end
+    end
+  end
 end
