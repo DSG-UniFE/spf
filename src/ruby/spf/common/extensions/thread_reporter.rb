d063a651fac8a910a78b24d7862cf5bcc195433f
giulio <giulio.ribe@gmail.com>
2016-12-30 12:20:43 +0100

    Added thread reporter

---
+++ src/ruby/spf/common/extensions/thread_reporter.rb
@@ -0,0 +1,15 @@
+class Thread
+  alias old_new new
+
+  def new(*args, &block)
+    old_new(*args) do |*bargs|
+      begin
+        block.call(*bargs)
+      rescue Exception => e
+        raise if Thread.abort_on_exception || Thread.current.abort_on_exception
+        puts "Thread for block #{block.inspect} terminated with exception: #{e.message}"
+        puts e.backtrace.map {|line| "  #{line}"}
+      end
+    end
+  end
+end
