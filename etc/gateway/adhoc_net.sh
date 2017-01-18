32856f41a7336ed3f019b205891353e999dfeb15
Alexdp74 <alessandro.morelli.1987@gmail.com>
2017-01-18 13:40:54 +0100

    Now script requires the name of the network interface that will set to ad-hoc mode and the IP address that will assign to that interface

--- etc/gateway/adhoc_net.sh
+++ etc/gateway/adhoc_net.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
-RE1=$(echo "\bID\b=(\w+)")
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+
+if test $# -ne 2
+then
+    echo "Error! Correct usage is:"
+    echo "/path/to/$0 <INTERFACE> <IP_ADDRESS>"
+    exit 1
+fi
+
+if ! `ifconfig $1 1> /dev/null`
+then
+    echo "Specified network interface $1 is not valid"
+    exit 2
+fi
+IF_NAME=$1
+shift
+
+if ! valid_ip $1
+then
+    echo "Specified IP address $1 is not valid"
+    exit 3
+fi
+IP_ADDR=$1
+shift
+
+RE=$(echo "\bID\b=(\w+)")
 OS_INFO=`cat /etc/os-release`
-if [[ $OS_INFO =~ $RE1 ]]
+if [[ $OS_INFO =~ $RE ]]
 then
     DISTRO=`echo ${BASH_REMATCH[1]}`
     echo "Detected distro: $DISTRO"
 else
-    echo ${BASH_REMATCH[1]}
     echo "Cannot detect the OS distro"
-    exit 1
+    exit 4
 fi
 
 case $DISTRO in
@@ -22,13 +63,13 @@ case $DISTRO in
         echo "Stopped NetworkManager service";;
     raspbian)
         sudo service networking stop
-        echo "Stopped networking service"
+        echo "Stopped networking service";;
     *)
         echo "Distro $DISTRO not supported!"
-        exit 2;;
+        exit 5;;
 esac
 
-sudo ip link set wlp3s0 down
-sudo iwconfig wlp3s0 mode ad-hoc essid DS_ADHOC channel 11
-sudo ip link set wlp3s0 up
-sudo ip addr add 192.168.1.9/24 dev wlp3s0
+sudo ip link set $IF_NAME down
+sudo iwconfig $IF_NAME mode ad-hoc essid DS_ADHOC channel 11
+sudo ip link set $IF_NAME up
+sudo ip addr add $IP_ADDR/24 dev $IF_NAME
