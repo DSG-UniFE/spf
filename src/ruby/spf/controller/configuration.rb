1f0cfe71f160d09bc13708e9fa59555809278cd9
giulio <giulio.ribe@gmail.com>
2017-01-18 18:30:43 +0100

    Updated configuration for read new params from file

--- src/ruby/spf/controller/configuration.rb
+++ src/ruby/spf/controller/configuration.rb
@@ -1,25 +1,34 @@
 require 'spf/common/validate'
 require 'spf/common/logger'
+require 'spf/common/exceptions'
+
 
 module SPF
   module Controller
-
     class Configuration
       include SPF::Logging
 
-      attr_reader :pigs
+      attr_reader :config
 
       private
 
         def initialize
-          @pigs = []
+          @config = Hash.new
+        end
+
+        def configuration(config)
+          @config = config
+          @config[:requests_port] = config[:requests_port].to_i
+          @config[:manager_port] = config[:manager_port].to_i
         end
 
       public
 
-        def validate
+        def validate?
           # check coordinates are valid, IPs are valid, ports are valid
-          @pigs.delete_if {|pig| !SPF::Common::Validate.pig? pig }
+          SPF::Common::Validate.ip? @config[:host] and
+            SPF::Common::Validate.port? @config[:requests_port] and
+            SPF::Common::Validate.port? @config[:manager_port]
         end
 
         def self.load_from_file(filename)
@@ -33,10 +42,10 @@ module SPF
           conf.instance_eval(File.new(filename, 'r').read)
 
           # validate and finalize configuration
-          conf.validate
+          raise SPF::Common::Exceptions::ConfigurationError, "Controller: Configuration not passed validate!" unless conf.validate?
 
           # return new object
-          conf.pigs
+          conf.config
         end
 
     end
