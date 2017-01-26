788d07b3ac109235456a0452ff916fc44448989a
giulio <giulio.ribe@gmail.com>
2017-01-26 10:48:50 +0100

    Updated validate class for new configuration

--- src/ruby/spf/common/validate.rb
+++ src/ruby/spf/common/validate.rb
@@ -12,6 +12,7 @@ module SPF
       # @@PROCESS_FOLDER = File.join('src', 'ruby', 'spf', 'gateway', 'processing-strategies')
       @@SERVICES_FOLDER = File.expand_path(File.join(File.dirname(__FILE__), '..', 'gateway', 'service-strategies'))
       @@PROCESS_FOLDER = File.expand_path(File.join(File.dirname(__FILE__), '..', 'gateway', 'processing-strategies'))
+      @@APPLICATION_CONFIG_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'etc', 'controller', 'app_configurations'))
 
       def self.ip?(ip)
         (ip.eql? "localhost") or (ip =~ Regexp.union([Resolv::IPv4::Regex, Resolv::IPv6::Regex]) ? true : false)
@@ -36,11 +37,14 @@ module SPF
           Validate.latitude? pig[:gps_lat] and Validate.longitude? pig[:gps_lon]
       end
 
-      def self.conf?(name, opt)
+      def self.conf?(app_name, opt)
         return false unless (opt.keys & @@KEYS).any?
 
         services = Dir.entries(@@SERVICES_FOLDER)
         process = Dir.entries(@@PROCESS_FOLDER)
+        applications = Dir.entries(@@APPLICATION_CONFIG_DIR)
+
+        return false unless applications.include? app_name
 
         opt.keys.each do |key|
           case key
@@ -55,37 +59,42 @@ module SPF
 
           when :service_policies
             opt[key].keys.each do | service |
-              case service
+              opt[key][service].each do | key_service, value |
+                case key_service
 
-              when :find_text
-                return false unless process.include?(opt[key][service][:processing_pipeline].to_s + "_processing_strategy.rb")
+                when :processing_pipeline
+                  return false unless process.include?(value.to_s + "_processing_strategy.rb")
 
-                return false unless opt[key][service][:filtering_threshold].between?(0, 1)
+                when :filtering_threshold
+                  return false unless value.between?(0, 1)
 
-                return false unless opt[key][service][:uninstall_after] >= 0
+                when :expire_after
+                  return false unless value >= 0
 
-                return false unless opt[key][service][:on_demand] == true or opt[key][service][:on_demand] == false
+                when :on_demand
+                  return false unless value == true or value == false
+                  unless value
+                     return false unless opt[key][service][:uninstall_after] >= 0
+                  end
+                when :uninstall_after
+                  next
 
-                return false unless @@DISTANCE_TYPES.include?(opt[key][service][:distance_decay][:type])
-                return false unless opt[key][service][:distance_decay][:max] >= 0
+                when :distance_decay
+                  return false unless @@DISTANCE_TYPES.include?(value[:type])
+                  return false unless value[:max] >= 0
 
-              when :audio_info
-                return false unless process.include?(opt[key][service][:processing_pipeline].to_s + "_processing_strategy.rb")
+                when :time_decay
+                  return false unless @@DISTANCE_TYPES.include?(value[:type])
+                  return false unless value[:max] >= 0
 
-                return false unless opt[key][service][:filtering_threshold].between?(0, 1)
-
-                return false unless opt[key][service][:on_demand] == true or opt[key][service][:on_demand] == false
-
-                return false unless @@DISTANCE_TYPES.include?(opt[key][service][:time_decay][:type])
-                return false unless opt[key][service][:time_decay][:max] >= 0
-              else
-                return false
+                else
+                    return false
+                end
               end
-
             end
 
           when :dissemination_policy
-            return false unless opt[key][:subscription].is_a? String
+            return false unless opt[key][:subscription].eql? app_name
             return false unless opt[key][:retries] >= 0
             return false unless opt[key][:wait] >= 0
 
@@ -102,6 +111,7 @@ module SPF
         end
 
         return true
+
       rescue
         return false
       end
