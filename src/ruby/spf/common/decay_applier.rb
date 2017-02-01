e915c4c2c2bc5fec5cfa0f993adec454217232ac
Alexdp74 <alessandro.morelli.1987@gmail.com>
2017-02-01 22:46:00 +0100

    Fixed method apply_decay to use dynamic method call via send() to choose the decay type

--- src/ruby/spf/common/decay_applier.rb
+++ src/ruby/spf/common/decay_applier.rb
@@ -3,21 +3,26 @@ module SPF
     module DecayApplier
       
       def self.apply_decay(value, rules)
-          # enforce maximum value if needed
-          raise SPF::Common:OutOfRangeException, "#{self.class.name}: Parameter out of range : #{value}" unless value >= 0
-          return 0.0 if value > rules[:max]
-
+        # enforce maximum value if needed
+        raise SPF::Common:OutOfRangeException,
+          "#{self.class.name}: Parameter out of range: #{value}" unless value >= 0
+        return 0.0 if value >= rules[:max]
+        
+        begin
           # apply decay according to the requested type
-          decay_modifier = case rules[:type]
-          when :exponential
-            Math.exp( value / (Math::E * (value - rules[:max])))
-          when :linear
-            1.0 - ( value / rules[:max].to_f )
-          else
-            1.0 # default is no decay at all
-          end 
-
+          self.send(rules[:type], value, rules[:max].to_f)
+        rescue NoMethodError => e
+          1.0
         end
+      end
+      
+      def self.exponential(value, max)
+        Math.exp(value / (Math::E * (value - max)))
+      end
+      
+      def self.linear(value, max)
+        1.0 - (value / max)
+      end
 
     end
   end
