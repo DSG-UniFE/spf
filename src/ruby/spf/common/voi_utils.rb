90cbc4684b2459106f03e763df0529dcffadfa91
Alexdp74 <alessandro.morelli.1987@gmail.com>
2017-02-03 15:13:07 +0100

    Added method description

--- src/ruby/spf/common/voi_utils.rb
+++ src/ruby/spf/common/voi_utils.rb
@@ -3,7 +3,15 @@ require 'spf/common/gps'
 module SPF
   module Common
     module VoiUtils
-
+      
+      # Calculate the VoI from the parameters.
+      #
+      # @param io_quality [Float] The quality of the IO.
+      # @param app_priority [Float] The priority of the application.
+      # @param norm_reqs [Float] The number of requestors interested in the IO, normalized by  
+      #                          the highest number of requests received by all services.
+      # @param rds_time [Float] The time decay factor.
+      # @param prox_d [Float] The distance decay factor
       def calculate_voi(io_quality, app_priority, norm_reqs, rds_time, prox_d)
         voi = io_quality * app_priority * norm_reqs * rds_time * prox_d
         puts "VoI: #{voi}"
@@ -11,11 +19,18 @@ module SPF
         voi
       end
 
+      # Returns the most recent (greatest) time in the Array passed in as parameter.
+      #
+      # @param times_array [Array] An array of Times.
       def most_recent_time(times_array)
-        # most recent time is the max time
         times_array.max
       end
 
+      # Returns the location of the requestor that is the closest to a reference position
+      #
+      # @param locations_array [Array] An array of GPS coordinates.
+      # @param ref_position [Hash] An Hash that contains the GPS coordinates 
+      #                            of the reference position.
       def closest_requestor_location(locations_array, ref_position)
         min_distance = Float::INFINITY
         min_location = nil
@@ -27,6 +42,11 @@ module SPF
         min_location
       end
 
+      # Returns the distance of the closest requestor from a reference position
+      #
+      # @param locations_array [Array] An array of GPS coordinates.
+      # @param ref_position [Hash] An Hash that contains the GPS coordinates 
+      #                            of the reference position.
       def distance_to_closest_requestor(locations_array, ref_position)
         min_distance = Float::INFINITY
         locations_array.each do |l|
