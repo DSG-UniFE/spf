1c0cce05313dc7022e30343d5092547c6da1b51f
Alexdp74 <alessandro.morelli.1987@gmail.com>
2017-02-08 09:40:01 +0100

    Converted exp time to ms; temporarily added a puts for debugging purposes

--- src/ruby/spf/gateway/service.rb
+++ src/ruby/spf/gateway/service.rb
@@ -36,7 +36,8 @@ module SPF
         @tau = service_conf[:filtering_threshold].nil? ? @@DEFAULT_TAU : service_conf[:filtering_threshold]
         @on_demand = service_conf[:on_demand]
         @max_idle_time = service_conf[:uninstall_after]
-        @response_expiration_time = service_conf[:expire_after]
+        @response_expiration_time = service_conf[:expire_after] * 1000    # in milliseconds
+        puts "service #{name.to_s} - response_expiration_time: #{response_expiration_time}"
         @service_strategy = service_strategy
         @application = application
         @is_active = false
