fb0df997482f835ec7be400750336ed302a77595
giulio <giulio.ribe@gmail.com>
2018-03-05 15:57:18 +0100

    Rename simple_client

--- src/ruby/bin/simple_client-disservice.rb
+++ src/ruby/bin/simple_client-disservice.rb
@@ -17,12 +17,12 @@ require 'spf/common/validate'
 require 'spf/common/extensions/fixnum'
 require 'spf/common/extensions/thread_reporter'
 
-java_import 'us.ihmc.aci.dspro2.DSProProxyListener'
-java_import 'us.ihmc.aci.dspro2.AsyncDSProProxy'
+java_import 'us.ihmc.aci.disServiceProxy.DisseminationServiceProxyListener'
+java_import 'us.ihmc.aci.disServiceProxy.AsyncDisseminationServiceProxy'
 
 
 class ResponseListener
-  java_implements DSProProxyListener
+  java_implements DisseminationServiceProxyListener
 
   attr_reader :n_receive_requests
 
@@ -35,17 +35,15 @@ class ResponseListener
     @id_list = Array.new
   end
 
-  java_signature 'dataArrived (String dsproId, String groupName, String objectId,\
-                                String instanceId, String annotatedObjMsgId,\
-                                String mimeType, byte[] data, short chunkNumber,\
-                                short totChunksNumber, String callbackParameters)'
-  def dataArrived (dsproId, groupName, objectId, instanceId,
-                   annotatedObjMsgId, mimeType, data, chunkNumber, totChunksNumber, callbackParameters)
-    puts "\nReceived new IO with ID '#{dsproId}'"
+  java_signature 'dataArrived (String msgId, String sender, String groupName, int seqNum, String objectId,\
+                               String instanceId, String mimeType, byte[] data, int metadataLength,\
+                               short tag, byte priority, String queryId)'
+  def dataArrived (msgId, sender, groupName, seqNum, objectId, instanceId,
+                   mimeType, data, metadataLength, tag, priority, queryId)
+    puts "\nReceived new IO with ID '#{msgId}' from '#{sender}'"
     puts "Application: #{groupName}"
     puts "ObjectID: #{objectId}"
-    puts "ChunkNumber: #{chunkNumber}"
-    puts "TotChunksNumber: #{totChunksNumber}"
+    puts "IstanceID: #{instanceId}"
 
     if mimeType.eql? "text/plain"
       puts "Data: #{data}"
@@ -53,10 +51,10 @@ class ResponseListener
       puts "Impossible to visualize data with MIME type #{mimeType}"
     end
 
-    if @id_list.include? dsproId
-      puts "\nERROR: message with id '#{dsproId}' already received!!!"
+    if @id_list.include? msgId
+      puts "\nERROR: message with id '#{msgId}' already received!!!"
     else
-      @id_list << dsproId
+      @id_list << msgId
 
       @n_receive_requests += instanceId.split(";")[1].to_i
 
@@ -73,79 +71,41 @@ class ResponseListener
     end
   end
 
-  java_signature 'void metadataArrived (String dsproId, String groupName, String referredDataObjectId,\
-                                        String referredDataInstanceId, String xMLMetadata,\
-                                        String referredDataId, String queryId)'
-  def metadataArrived (dsproId, groupName, referredDataObjectId, 
-                        referredDataInstanceId, xMLMetadata, referredDataId, queryId)
-        puts "\nReceived new metadata with ID '#{dsproId}'"
-        puts "Application: #{groupName}"
-        puts "ObjectID: #{referredDataObjectId}"
-        puts "ReferredDataId #{referredDataId}"
-        puts "XMLMetadata: \n #{xMLMetadata}"
-        puts "*** Requesting data ***"
-        data_wrapper = @ds_proxy.getData(referredDataId)
-        unless data_wrapper.nil? 
-          data = data_wrapper._data
-          # get the mimeType from the XML 
-          mimeType = "text/plain"
-          puts "Data from getData #{data} #{mimeType}"
-          if mimeType.eql? "text/plain"
-            puts "Data: #{data}"
-          else
-            puts "Impossible to visualize data with MIME type #{mimeType}"
-          end
-      
-          if @id_list.include? dsproId
-            puts "\nERROR: message with id '#{dsproId}' already received!!!"
-          else
-            @id_list << dsproId
-      
-            @n_receive_requests += referredDataInstanceId.split(";")[1].to_i
-      
-            if @requests.has_key? groupName.to_sym
-              @requests[groupName.to_sym][:end] << [Time.now.strftime("%H:%M:%S.%L"), referredDataInstanceId]
-            else
-              puts "\nReceived message with a group name '#{groupName}' not present in @requests"
-            end
-          end
-          puts "Received so far #{@n_receive_requests} responses out of #{@n_requests}"
-          if @n_receive_requests >= @n_requests
-            unsubscribe()
-          end
-        end
+  java_signature 'void chunkArrived (String msgId, String sender, String groupName, int seqNum, String objectId,\
+                                     String instanceId, String mimeType, byte[] data, short nChunks, short totNChunks,\
+                                     String chunkedMsgId, short tag, byte priority, String queryId)'
+  def chunkArrived (msgId, sender, groupName, seqNum, objectId, instanceId, mimeType, data,
+                    nChunks, totNChunks, chunkedMsgId, tag, priority, queryId)
+    raise "chunkArrived: METHOD NOT IMPLEMENTED!"
   end
 
-  java_signature 'boolean pathRegistered (NodePath path, String nodeId, String teamId, String mission)'
-  def pathRegistered (path, nodeId, teamId, mission)
-    raise "pathRegistred: METHOD NOT IMPLEMENTED"
+  java_signature 'void metadataArrived (String msgId, String sender, String groupName, int seqNum, String objectId,\
+                                        String instanceId, String dataMimeType, byte[] metadata,\
+                                        boolean dataChunked, short tag, byte priority, String queryId)'
+  def metadataArrived (msgId, sender, groupName, seqNum, objectId, instanceId, dataMimeType,
+                       metadata, dataChunked, tag, priority, queryId)
+    raise "metadataArrived: METHOD NOT IMPLEMENTED!"
   end
 
-  java_signature 'boolean positionUpdated (float latitude, float longitude, float altitude, String nodeId)'
-  def positionUpdated (latitude, longitude, altitude, nodeId)
-    raise "positionUpdated: METHOD NOT IMPLEMENTED!"
-  end
-
-  java_signature 'void newNeighbor (String peerID)'
-  def newNeighbor (peerID)
-    raise "newNeighbor: METHOD NOT IMPLEMENTED!"
-  end
-  
-  java_signature 'void deadNeighbor (String peerID)'
-  def deadNeighbor (peerID)
-    raise "deadNeighbor: METHOD NOT IMPLEMENTED!"
+  java_signature 'void dataAvailable (String msgId, String sender, String groupName, int seqNum, String objectId,\
+                                      String instanceId, String mimeType, String id, byte[] metadata,\
+                                      short tag, byte priority, String queryId)'
+  def dataAvailable (msgId, sender, groupName, seqNum, objectId, instanceId,
+                     mimeType, id, metadata, tag, priority, queryId)
+    raise "dataAvailable: METHOD NOT IMPLEMENTED!"
   end
 
   def unsubscribe()
-    # Terminate current thread
-    @ds_proxy.requestTermination
+    # unsubscribe from the group and terminate current thread
+    @ds_proxy.unsubscribe(@app_name)
+    @ds_proxy.asynchThreadDone
   end
 
 end
 
 
 if ARGV.size != 3
-  abort("ERROR!!! Correct usage is: ruby dspro_simple_client.rb <IP_CONTROLLER> <APPLICATION_NAME> <#REQUESTS>")
+  abort("ERROR!!! Correct usage is: ruby simple_client.rb <IP_CONTROLLER> <APPLICATION_NAME> <#REQUESTS>")
 end
 
 unless SPF::Common::Validate.ip?(ARGV[0])
@@ -170,17 +130,15 @@ N_REQUESTS = ARGV[2].to_i
 
 requests = Hash.new
 
-proxy = AsyncDSProProxy.new(7843.to_java(:short), 60000.to_java(:long))
+proxy = AsyncDisseminationServiceProxy.new(7843.to_java(:short), 60000.to_java(:long))
 responseListener = ResponseListener.new(proxy, APPLICATION_NAME, requests, N_REQUESTS)
 begin
-  rc = proxy.init
-  if rc != 0
-    raise "*** #{self.class.name}: DSProProxy init failed - proxy down? ***"
-  end
+  proxy.init
+  proxy.subscribe(APPLICATION_NAME, 1.to_java(:byte), true.to_java(:boolean), true.to_java(:boolean), false.to_java(:boolean))
+  proxy.registerDisseminationServiceProxyListener(responseListener)
   t = Java::JavaLang::Thread.new { proxy.run }
   t.start
 
-  proxy.registerDSProProxyListener(responseListener)
   requests[APPLICATION_NAME.to_sym] = Hash.new
   requests[APPLICATION_NAME.to_sym][:start] = Array.new
   requests[APPLICATION_NAME.to_sym][:end] = Array.new
@@ -193,7 +151,7 @@ begin
       socket.puts "REQUEST participants/find_text"
       socket.puts "User Giulio;40.010101,10.010101;find 'water'"
     when "surveillance"
-      socket.puts "REQUEST surveillance/basic"
+      socket.puts "REQUEST surveillance/surveillance"
       socket.puts "User Giulio;40.010101,10.010101;count objects"
     else
       abort("ERROR: application not present in case/when!")
@@ -216,8 +174,7 @@ begin
   # wait up to 5 minutes for a response to arrive, and then exit
   t.join(5 * 60 * 1000)
 
-  #terminate proxy
-  proxy.requestTermination()
+  responseListener.unsubscribe()
 
   if responseListener.n_receive_requests == N_REQUESTS
     puts "\nOh yeah, received #{responseListener.n_receive_requests} requests"
@@ -261,11 +218,11 @@ begin
   puts "\nBye"
   exit
 rescue java.net.ConnectException => e
-  Kernel.abort("ERROR: unable to connect to the DSProProxy instance - proxy down?")
+  Kernel.abort("ERROR: unable to connect to the DisServiceProxy instance - proxy down?")
 rescue Errno::ECONNREFUSED => e
   Kernel.abort("ERROR: unable to open a TCP connection to #{HOST}:#{PORT} - SPF Controller down?")
 rescue => e
   puts e.message
   puts e.backtrace
-  Kernel.abort("ERROR: unknown error when trying to connect to the DSProProxy instance")
+  Kernel.abort("ERROR: unknown error when trying to connect to the DisServiceProxy instance")
 end
